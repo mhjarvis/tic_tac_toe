64752884263e621ab878301f240387e57f78622e
mhjarvis <markus.h.jarvis@gmail.com>
2021-03-03 09:22:50 -0700

    Updated boardgame object

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 'use strict'
 
-const Player = (name, marker) => {
-    return {name, marker};
-};
-
 //---------------------------------GAMEBOARD----------------------------------------
 const gameboard = (() => {                  //build gameboard using module
     
-    let board = ["", "", "", "", "", "", "", "", ""];
+    const board = ["", "", "", "", "", "", "", "", ""];
+    let container = document.getElementById('container');
 
     const buildGameBoard = () => {
+        console.log("teeeee");
         for(let i = 0; i < 9; i++) {
-            let container = document.getElementById('container');
             const div = document.createElement('div');
             container.appendChild(div);
             div.className = "box";
             div.id = ("box" + i);
-            div.innerText = board[i];
+            //div.innerText = board[i];
         }
     }
-    function resetBoard() {
-        board = [];
-        console.log({board});
-    }
-    return (buildGameBoard(), resetBoard());
 
+    const resetBoard = () => {
+        //for(let  i = 0; i < board.length; i++) {
+          //  board[i] = "x";
+            const x = document.getElementsByClassName("box");
+            for(let i = 0; i < x.length; i++) {
+                board[i] = "e";
+                x[i].innerText = board[i];
+            }
+            console.log(board);
+        //}
+    };
+    // function resetBoard() {
+    //     board = ["", "", "", "", "", "", "", "", ""];        
+    //     console.log({board});
+    // };
+
+    // const b = () => {
+    //     board = ["", "", "", "", "", "", "", "", ""];    
+    //         alert("HHHHH");
+    // }
+    
+    return {buildGameBoard, resetBoard}; //b());
+})();
 
     // function buildGameBoard() {
     //     let container = document.getElementById('container');
@@ -75,11 +90,17 @@ const gameboard = (() => {                  //build gameboard using module
 
 
 
-})();
 
+
+const Player = (name, marker) => {
+    return {name, marker};
+};
 
 //------------------------------------GAME CONTROLLER-----------------------------------
 const gameController = (() => {
+
+    gameboard.buildGameBoard();
+    
 
     const player1 = Player("Player 1", "X");
     const player2 = Player("Player 2", "O");
@@ -113,11 +134,11 @@ const gameController = (() => {
     };
 
     const playAgainButton = document.querySelector('#playAgain');
-    playAgainButton.addEventListener('click', resetAll);
+    playAgainButton.addEventListener('click', () => {
+        gameboard.resetBoard();
+    });
 
-    function resetAll() {
-        gameboard.resetBoard;
-    }
+
 
 
 })();
