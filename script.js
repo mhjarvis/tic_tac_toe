42fffd3bd555cd466f00e25ca66eced2a0c98ae4
mhjarvis <markus.h.jarvis@gmail.com>
2021-03-03 14:17:22 -0700

    updated Gameboard object with necessary functions; updated gameController object

--- script.js
+++ script.js
@@ -1,113 +1,80 @@
 'use strict'
 
-//---------------------------------GAMEBOARD----------------------------------------
-const gameboard = (() => {                  //build gameboard using module
+//---------------------------------GAMEBOARD OBJECT----------------------------------
+const Gameboard = (() => {     //build gameboard using module pattern
     
     const board = ["", "", "", "", "", "", "", "", ""];
-    let container = document.getElementById('container');
+    const container = document.getElementById('container');
+    const getBoxClass = document.getElementsByClassName("box");
 
     const buildGameBoard = () => {
-        console.log("teeeee");
         for(let i = 0; i < 9; i++) {
             const div = document.createElement('div');
             container.appendChild(div);
             div.className = "box";
-            div.id = ("box" + i);
-            //div.innerText = board[i];
+            div.id = (i);
+        };
+    };
+    const resetBoard = () => {
+        for(let i = 0; i < board.length; i++) {
+            board[i] = "";
+            getBoxClass[i].innerText = board[i];
+        };
+        console.log(board);
+    };
+    const getGameBoard = () => {
+        return board;
+    }
+    const updateGameBoard = (index, marker) => {
+        board[index] = marker;
+        for(let i = 0; i < 9; i++) {
+            getBoxClass[i].innerText = board[i];
         }
     }
-
-    const resetBoard = () => {
-        //for(let  i = 0; i < board.length; i++) {
-          //  board[i] = "x";
-            const x = document.getElementsByClassName("box");
-            for(let i = 0; i < x.length; i++) {
-                board[i] = "e";
-                x[i].innerText = board[i];
-            }
-            console.log(board);
-        //}
+    return {
+        buildGameBoard,
+        resetBoard,
+        getGameBoard,
+        updateGameBoard
     };
-    // function resetBoard() {
-    //     board = ["", "", "", "", "", "", "", "", ""];        
-    //     console.log({board});
-    // };
-
-    // const b = () => {
-    //     board = ["", "", "", "", "", "", "", "", ""];    
-    //         alert("HHHHH");
-    // }
-    
-    return {buildGameBoard, resetBoard}; //b());
 })();
 
-    // function buildGameBoard() {
-    //     let container = document.getElementById('container');
-        
-    //     for(let i = 0; i < 9; i++) {
-    //         board.push("");
-    //     }
-    //     for(let i = 0; i < 9; i++) {
-    //         const div = document.createElement('div');
-    //         container.appendChild(div);
-    //         div.className = "box";
-    //         div.id = ("box" + i);
-    //         div.innerText = board[i];
-    //     }
-    //     console.log("Board built");
-    // }
-
-//event listeners
-
-
-    // const box = document.querySelectorAll('.box');
-    // box.forEach((box) => {
-    //     box.addEventListener('click', () => {
-    //         addMarker(box.id);
-    //     })
-    // })
-    // function addMarker(id) {
-    //     const box = document.getElementById(id);
-    //     if(box.innerText == "x")
-    //         box.innerText = "O";
-    //     else
-    //         return;
-    // };
-
-
-
-
-    // const playAgainButton = document.querySelector('#playAgain');
-    // playAgainButton.addEventListener('click', resetAll);
-
-    // function resetAll() {
-        
-    //     console.log(board);
-    // }
-
-    //change marker display in clicked box
-
-
-
-
-
-
-const Player = (name, marker) => {
+//---------------------------------PLAYER OBJECT------------------------------------
+const Player = (name, marker) => {      //build object with factory function    
     return {name, marker};
 };
 
-//------------------------------------GAME CONTROLLER-----------------------------------
-const gameController = (() => {
+//---------------------------------GAME CONTROLLER-----------------------------------
+const gameController = (() => {         //build game controller with module pattern
 
-    gameboard.buildGameBoard();
+    Gameboard.buildGameBoard();
     
-
     const player1 = Player("Player 1", "X");
     const player2 = Player("Player 2", "O");
+    let currentPlayer = whoGoesFirst();            //'player1' or 'player2'
+    console.log(currentPlayer);
 
-    function playerTurn() {
-        let startingPlayer = whoGoesFirst();
+    //add event listener for each div (box)
+    const box = document.querySelectorAll('.box');
+    box.forEach((box) => {
+        box.addEventListener('click', () => {
+            if(box.innerText == ""){                //test box for current value
+                let currentPlayer = getCurrentPlayer();
+                addMarker(box.id, currentPlayer);
+                updateCurrentPlayer();
+            } else {
+                return;
+            }
+        })
+    })
+    //on click, call updateGameboard and update display
+    function addMarker(id, player) {
+        Gameboard.updateGameBoard(id, player.marker);
+    };
 
+    //
+    function getCurrentPlayer() {
+        return currentPlayer;
     }
     //function to determine order of play
     function whoGoesFirst() {
@@ -118,24 +85,20 @@ const gameController = (() => {
             return player2;
         }
     }
+    function updateCurrentPlayer() {
+        let temp = getCurrentPlayer();
+        if(temp == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
 
-    const box = document.querySelectorAll('.box');
-    box.forEach((box) => {
-        box.addEventListener('click', () => {
-            addMarker(box.id);
-        })
-    })
-    function addMarker(id) {
-        const box = document.getElementById(id);
-        if(box.innerText == "")
-            box.innerText = "O";
-        else
-            return;
-    };
+
 
     const playAgainButton = document.querySelector('#playAgain');
     playAgainButton.addEventListener('click', () => {
-        gameboard.resetBoard();
+        Gameboard.resetBoard();
     });
 
 
